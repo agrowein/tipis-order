ba4661f2993d707749fb6de09837f0dcf4ea214b
agrowein <geo.harri40@gmail.com.com>
2021-10-09 20:57:55 +0300

    Added spectrum method to Function

--- chebotarev/lab1/function.py
+++ chebotarev/lab1/function.py
@@ -1,26 +1,44 @@
 import numpy as np
+import numpy.fft as fft
 
-class Function: 
+class Function:
     '''
     Класс рассчитывает значения для переданной функции.
     '''
-
     def __init__(self, f): 
         self.f = f
         self.sampling_step = 0.0001 # шаг дискретизации 
+        self.maxfreq = int(1 / (self.sampling_step * 2)) 
+        self.values = None
 
     def calculate(self, range, freq=1):
         '''
         Метод рассчета значений функции от времени
         Args:
-            range (tuple): Диапазон значений.
-            freq (number, optioan): Частота. Defaults to 1.
+           range (tuple): Диапазон значений.
+           freq (number, optioan): Частота. Defaults to 1.
 
         Returns:
-            tuple: {x,y} массивы значений на диапазоне по X и Y
+           tuple: {x,y} массивы значений на диапазоне по X и Y
         '''
-          
         x = np.arange(range[0], range[1], self.sampling_step)
         y = self.f(x * 2 * np.pi * freq)
-        return (x, y)
+        self.values = (x, y)
+        return self.values
+
+    def spectrum(self):
+        '''
+        Метод рассчета спектра функции
+
+        Returns:
+           tuple: {x,y} массивы значений на диапазоне по X и Y
+        '''
+        if not self.values:
+            raise Exception('Значения функции не рассчитаны! \n Рассчитайте значения функции через метод calculate()')
+
+        amplitude = np.abs(fft.fft(self.values[1]))
+        amplitude = amplitude[:len(amplitude) // 2] 
+        freq = (np.linspace(0, self.maxfreq, len(amplitude))[:50], amplitude[:50])
+        return freq
         
+    
--- chebotarev/lab1/program.py
+++ chebotarev/lab1/program.py
@@ -1,10 +1,38 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from function import Function
 
+
+def draw_chart(func, range_, freq):
+    temp = func.calculate(range_, freq)
+    spectrum = func.spectrum()
+
+    plt.subplot(2, 1, 1)
+    plt.title(f'Сигнал {freq}Гц')
+    plt.plot(temp[0], temp[1])
+    plt.grid()
+
+    plt.subplot(2, 1, 2).set_xticks(range(0, 50))
+    plt.title(f'Частотный спектр сигнала {freq}Гц')
+    plt.plot(spectrum[0], spectrum[1])
+    plt.grid()
+    
+    plt.show()
+
+
 def main():
-    test = Function(np.sin)
-    res = test.calculate((0, 1))
-    print(res)
+    # sin = Function(np.sin)
+    # draw_chart(sin, (0, 2), 1) # для 1 гц
+    # draw_chart(sin, (0, 2), 2) # для 2 гц
+    # draw_chart(sin, (0, 2), 4) # для 4 гц
+    # draw_chart(sin, (0, 2), 8) # для 8 гц
+
+    # cb = lambda x: np.sign(np.sin(x))
+    # sing = Function(cb)
+    # draw_chart(sing, (0, 2), 1) # для 1 гц
+    # draw_chart(sing, (0, 2), 2) # для 2 гц
+    # draw_chart(sing, (0, 2), 4) # для 4 гц
+    # draw_chart(sing, (0, 2), 8) # для 8 гц
     
 
 if __name__ == '__main__':
